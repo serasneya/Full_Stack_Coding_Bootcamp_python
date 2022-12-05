08ef72853b542989b142d072aaf2da329ef6730a
serasneya <119518113+serasneya@users.noreply.github.com>
2022-12-05 18:07:36 +0000

    Add files via upload

---
+++ Exosday1.py
@@ -0,0 +1,58 @@
+#Exercice 1#
+print("Hello World\n"*4)
+#fin de l'exercice 1#
+
+#Exercice 2#
+x=pow(99, 3)
+y=x*8
+print(x)
+print(y)
+
+#Exercice 3#
+#5 < 3 False#
+#3 == 3 True#
+# 3 == "3" False
+# "3">3 False
+# "Hello" == "hello" False#
+
+#Exercice 4#
+hp="I have a hp"
+print(hp)
+
+#Exercice 5
+name="NEYA"
+age=33
+shoe_size=41
+info="NEYA is 33 years old and like shoes size 33"
+print(info)
+
+
+#Exercice 6
+a=15
+b=10
+if a>b:
+    print("Hello World")
+
+#Exercice 7
+num = int(input("Enter a number: "))
+mod = num % 2
+if mod > 0:
+    print("ODD")
+else:
+    print("Even")
+
+
+#Exercice 8
+name = str(input("Enter your name: "))
+if name=="NEYA":
+    print("We are parents")
+else:
+    print("Nice to meet you")
+
+#Exercice 9
+heig = int(input("Enter your height : "))
+inch = int(input("Enter your inch please: "))
+if inch>145:
+    print("You can ride")
+else:
+    print("You need to grow more")
